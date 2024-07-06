f56452e28bceba824e9baa52dad7dd4c58ae2813
Pervej0 <mdparvez222khan@gmail.com>
2024-07-06 12:04:37 +0600

    jsonToken added

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -3,4 +3,5 @@ import "dotenv/config";
 export default {
   DATABASE_URL: process.env.DATABASE_URL,
   PORT: process.env.PORT,
+  PRIVATE_KEY: process.env.PRIVATE_KEy,
 };
--- src/app/modules/auth/auth.router.ts
+++ src/app/modules/auth/auth.router.ts
@@ -1,7 +1,14 @@
 import express from "express";
+import generateToken from "../../shared/generateToken";
 const app = express.Router();
 
 app.get("/login", (req, res) => {
+  const payload = {
+    email: "hellO@gmail.com",
+    role: "author",
+  };
+  const token = generateToken(payload);
+  console.log(token);
   res.json({ Message: "Hello" });
 });
 
